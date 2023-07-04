4da939783e49e4e61449674000ea53a4f0549017
kuldeep <kuldeep@tatvic.com>
2023-07-04 18:50:23 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TagmateAnalytics
+
+A description of this package.
---
+++ Tests/TagmateAnalyticsTests/TagmateAnalyticsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TagmateAnalytics
+
+final class TagmateAnalyticsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TagmateAnalytics().text, "Hello, World!")
+    }
+}
