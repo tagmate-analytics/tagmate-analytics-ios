db1351d4ac9cb984823f10ec47bd501be64ce1a6
kuldeep <kuldeep@tatvic.com>
2023-10-20 17:20:06 +0530

    updated .target in package

--- Package.swift
+++ Package.swift
@@ -21,6 +21,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TagmateAnalytics",
-            dependencies: [.product(name: "FirebaseAnalytics", package: "firebase-ios-sdk")])
+            dependencies: [.product(name: "FirebaseAnalytics", package: "firebase-ios-sdk")],
+            path: "Sources",
+              publicHeadersPath: "Public",
+              cSettings: [
+                  .headerSearchPath("Public")
+              ]
+        )
     ]
 )
